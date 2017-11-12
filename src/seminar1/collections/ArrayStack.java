51304da29e7a23089142a36b53ee5ad627c0a015
Михаил Нечаев <mikhail.nechaev@corp.mail.ru>
2017-11-13 01:19:03 +0300

    init

---
+++ src/seminar1/collections/ArrayStack.java
@@ -0,0 +1,80 @@
+package seminar1.collections;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class ArrayStack<Item> implements IStack<Item> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private Item[] elementData;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public ArrayStack() {
+        this.elementData = (Item[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    @Override
+    public void push(Item item) {
+        /* TODO: implement it */
+    }
+
+    @Override
+    public Item pop() {
+        /* TODO: implement it */
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void grow() {
+        /**
+         * TODO: implement it
+         * Если массив заполнился,
+         * то увеличить его размер в полтора раз
+         */
+    }
+
+    private void shrink() {
+        /**
+         * TODO: implement it
+         * Если количество элементов в четыре раза меньше,
+         * то уменьшить его размер в два раза
+         */
+    }
+
+    private void changeCapacity(int newCapacity) {
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ArrayStackIterator();
+    }
+
+    private class ArrayStackIterator implements Iterator<Item> {
+
+        private int currentPosition = size;
+
+        @Override
+        public boolean hasNext() {
+            return currentPosition != 0;
+        }
+
+        @Override
+        public Item next() {
+            return elementData[--currentPosition];
+        }
+
+    }
+
+}
